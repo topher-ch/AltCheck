2dfac5a40dd8c8c4ea495d4bbfda17e9e943a8c0
topher-ch <christopher.n.parnell@gmail.com>
2026-01-24 13:01:28 +1100

    Skeleton of UI buttons

--- obj/Debug/net9.0/alternator_analyser.AssemblyInfo.cs
+++ obj/Debug/net9.0/alternator_analyser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alternator_analyser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac766d5822aa43e7ff662c0dfbcafc71fcd312c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5fd8e74859e0d1665d7fcf24d7501979d64ad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("alternator_analyser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alternator_analyser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
